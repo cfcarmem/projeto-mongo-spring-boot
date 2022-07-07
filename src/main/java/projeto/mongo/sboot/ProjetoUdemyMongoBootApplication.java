405b3c557a53840ed3b38a256c16332ee58332ae
Carmem <cfcarmem@gmail.com>
2022-07-06 22:36:23 -0300

    first commit

---
+++ src/main/java/projeto/mongo/sboot/ProjetoUdemyMongoBootApplication.java
@@ -0,0 +1,13 @@
+package projeto.mongo.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUdemyMongoBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUdemyMongoBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/mongo/sboot/ProjetoUdemyMongoBootApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.mongo.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUdemyMongoBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
